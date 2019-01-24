1bda3aa64c4f6aa5066afe7209f0af93ee045e68
tjanicki <tjaygallery@gmail.com>
2019-01-24 20:08:35 +0100

    database

--- src/main/java/com/library/LibraryApplication.java
+++ src/main/java/com/library/LibraryApplication.java
@@ -2,12 +2,22 @@ package com.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-public class LibraryApplication {
+@PropertySource({"classpath:application.properties"})
+public class LibraryApplication extends SpringBootServletInitializer {
+
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LibraryApplication.class);
     }
 
 }
--- src/main/java/com/library/config/CoreConfiguration.java
+++ src/main/java/com/library/config/CoreConfiguration.java
@@ -1,5 +1,6 @@
 package com.library.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
